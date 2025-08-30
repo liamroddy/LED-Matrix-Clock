e468d8d6cfba474e78c4485272906191bd75f1eb
Liam Roddy <notrealemail@example.com>
2025-08-30 13:59:52 +0100

    cache focal_an_lae

--- schedule.py
+++ schedule.py
@@ -9,7 +9,18 @@ The format is a list of lists, where each inner list contains:
    (e.g. get_focal_an_lae() to get the Irish word of the day)
 """
 
+focal_an_lae = get_focal_an_lae()
+
+def get_todays_focal_an_lae():
+    return focal_an_lae
+
+def get_updated_focal_an_lae():
+    global focal_an_lae
+    focal_an_lae = get_focal_an_lae()
+    return focal_an_lae
+
 schedule = [
     ["0 * 25 12 *", "Nollaig shona daoibh!"], # 25th December, every hour on the hour
-    ["0/15 * * * *", get_focal_an_lae], # every 15 mins (if above does not takes priority)
+    ["15 0 * * *", get_updated_focal_an_lae], # every day at quarter past midnight
+    ["0/15 * * * *", get_todays_focal_an_lae], # every 15 mins (if above does not takes priority)
 ]
